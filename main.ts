0dd4d2d1e6dcf101b5e1fa0d48d572e4a46b9c19
LRJR1817 <l.richards25@psaschool.org>
2026-01-21 15:17:24 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 input.onButtonPressed(Button.A, function () {
-    if (true) {
-    	
-    }
+    MiniCar.motor(Motorlist.M1, Direction1.Forward, 0)
+    MiniCar.motor(Motorlist.M2, Direction1.Forward, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    MiniCar.motor(Motorlist.M1, Direction1.Backward, 0)
+    MiniCar.motor(Motorlist.M2, Direction1.Backward, 0)
 })
 basic.forever(function () {
-	
+    music.play(music.stringPlayable("C5 E C5 E C5 E C5 E ", 120), music.PlaybackMode.UntilDone)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_R, LED_color.red1)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_L, LED_color.blue1)
+    basic.pause(100)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_R, LED_color.blue1)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_L, LED_color.red1)
+    basic.pause(100)
 })
